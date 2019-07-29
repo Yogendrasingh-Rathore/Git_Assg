730f0577a95d1706cd7b3f56847913416310ee94
Yuvi <ysrathore1995@gmail.com>
2019-07-29 14:57:30 +0530

    multiply method added in Cal.c

--- Cal.c
+++ Cal.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 
-int Add(int a, int b)
+int multiply(int a, int b)
 {
-	return a+b;	
+	return a*b;	
 }
 
 void main()
 {
 	int r;
-	r = Add(5,7);
-	printf("Addition is : %d",r);
+	r = multiply(5,7);
+	printf("Multiplication is : %d",r);
 	
 }
