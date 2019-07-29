4da8bb7a5c26d64f6dcabcaa024da4d4bbde7038
Yuvi <ysrathore1995@gmail.com>
2019-07-29 17:14:56 +0530

    Changes in cal.c

--- Cal.c
+++ Cal.c
@@ -5,11 +5,6 @@ int Add(int a, int b)
 	return a+b;	
 }
 
-int subtract(int a, int b)
-{
-	return a-b;
-}
-
 int multiply(int a, int b)
 {
 	return a*b;	
@@ -20,9 +15,6 @@ void main()
 	int r;
 	r = Add(5,7);
 	printf("Addition is : %d",r);
-
-	r = subtract(5,7);
-	printf("Subtraction is : %d",r);
 
 	r = multiply(5,7);
 	printf("Multiplication is : %d",r);
