9502a0e2e20beb5693a07bf6ae6559741c7b94d8
Yuvi <ysrathore1995@gmail.com>
2019-07-29 14:50:23 +0530

    subtract method added to cal.c

--- Cal.c
+++ Cal.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 
-int Add(int a, int b)
+int add(int a, int b)
 {
-	return a+b;	
+	return a+b;
 }
 
 void main()
 {
 	int r;
-	r = Add(5,7);
+	r = add(5,7);
 	printf("Addition is : %d",r);
 	
 }
